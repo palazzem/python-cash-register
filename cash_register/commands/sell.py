19edff4eaa6d1ebcbd6d9b9f68e979f2fc1271c1
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-01 12:27:04 +0200

    simple list of sell commands

---
+++ cash_register/commands/sell.py
@@ -0,0 +1,23 @@
+"""
+Module that groups all commands related to sales.
+"""
+from .base import Command
+
+
+clear = Command(
+    'CLEAR',
+    'Clear the current action handled by the cash register',
+    'K',
+)
+
+sell = Command(
+    'SELL',
+    'Add a record to the current recipe',
+    '"{description}"{amount}{quantity}H1R',
+)
+
+close = Command(
+    'CLOSE',
+    'Close the current recipe',
+    '1T',
+)
