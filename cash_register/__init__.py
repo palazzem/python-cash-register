52ac769b0b987e9dc501f176423b43cbc8e7a03e
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-01 11:20:46 +0200

    bootstrap cash_register module

---
+++ cash_register/__init__.py
@@ -0,0 +1,12 @@
+"""
+Python cash register
+"""
+
+__title__ = 'Python cash register'
+__version__ = '0.1.0'
+__author__ = 'Emanuele Palazzetti'
+__license__ = 'BSD 2-Clause'
+__copyright__ = 'Copyright 2016 Emanuele Palazzetti'
+
+# Version synonym
+VERSION = __version__
