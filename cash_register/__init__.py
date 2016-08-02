fe82cff28a0b36d3217f2a10518e8010963ab027
Emanuele Palazzetti <emanuele.palazzetti@gmail.com>
2016-08-02 23:13:38 +0200

    bump version 0.1.1

--- cash_register/__init__.py
+++ cash_register/__init__.py
@@ -3,7 +3,7 @@ Python cash register
 """
 
 __title__ = 'Python cash register'
-__version__ = '0.1.0'
+__version__ = '0.1.1'
 __author__ = 'Emanuele Palazzetti'
 __license__ = 'BSD 2-Clause'
 __copyright__ = 'Copyright 2016 Emanuele Palazzetti'
